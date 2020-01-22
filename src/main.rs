a5fbcf4f19cce034a39511b6a35492b4642cff27
leviport <leviport377@gmail.com>
2020-01-21 21:08:35 -0700

    Make a switch hole!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use scad::*;
+
 fn main() {
-    println!("Hello, world!");
+    // my file which stores my output
+    let mut my_scad = ScadFile::new();
+
+    my_scad.set_detail(100);
+
+    let mut mx_switch_hole = scad!(Union);
+
+    mx_switch_hole.add_child(scad!(Cube(vec3(14.0, 14.0, 4.0))));
+
+    mx_switch_hole.add_child(
+        scad!(Translate(vec3(-1.0, 4.5, 0.0)); {
+            scad!(Cube(vec3(16.0, 5.0, 2.6)))
+        })
+    );
+
+    my_scad.add_object(mx_switch_hole.clone());
+
+    my_scad.write_to_file(String::from("output_file.scad"));
 }
