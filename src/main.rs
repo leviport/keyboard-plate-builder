1de688a093b5feb0bc8e74b10f2f330e2d605caf
leviport <leviport377@gmail.com>
2020-01-22 20:39:59 -0700

    Make it loopy

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,12 @@ use scad::*;
 
 fn main() {
     // my file which stores my output
-    let mut my_scad = ScadFile::new();
+    let mut output_scad = ScadFile::new();
 
-    my_scad.set_detail(100);
+    output_scad.set_detail(100);
 
     let mut mx_switch_hole = scad!(Union);
-
     mx_switch_hole.add_child(scad!(Cube(vec3(14.0, 14.0, 4.0))));
-
     mx_switch_hole.add_child(
         scad!(Translate(vec3(-1.0, 4.5, 0.0)); {
             scad!(Cube(vec3(16.0, 5.0, 2.6)))
@@ -18,15 +16,17 @@ fn main() {
 
     let mut plate_switch_holes = scad!(Union);
 
-    plate_switch_holes.add_child(mx_switch_hole.clone());
+    //plate_switch_holes.add_child(mx_switch_hole.clone());
 
-    plate_switch_holes.add_child(
-        scad!(Translate(vec3(19.0, 0.0, 0.0)); {
-            mx_switch_hole.clone()
-        })
-    );
+    for i in (0..15).rev() {
+        plate_switch_holes.add_child(
+            scad!(Translate(vec3(i as f32 * 19.0, 0.0, 0.0)); {
+                mx_switch_hole.clone()
+            })
+        );
+    }
 
-    my_scad.add_object(plate_switch_holes.clone());
+    output_scad.add_object(plate_switch_holes.clone());
 
-    my_scad.write_to_file(String::from("output_file.scad"));
+    output_scad.write_to_file(String::from("output_file.scad"));
 }
